c292d84c3fafc20af30b0083e8f165b336fb29ea
Aaron <kmikayelyan555@gmail.com>
2023-11-15 13:19:54 -0800

    final final

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,17 +8,19 @@ export default function Footer() {
   return (
     <div id="footer " className="overflow-hidden">
       <div className=" h-[600px] relative flex justify-center items-center">
-        <div className=" font-thin ">
+        <div className=" font-thin  ">
           <h3 className=" mr-[80px] ">747-332-7075</h3>
           <p className="mr-[80px]">Karen.h@deluxefurniture.company</p>
-        </div>
-        <Image src={Lamp} className=" absolute z-0    h-[600px] w-auto" />
-        <div className="ml-[50px]">
+
           <h3 className=" font-thin ">
             14545 Friar ST 302A <br />
             Van Nuys CA 91411
           </h3>
         </div>
+        <Image
+          src={Lamp}
+          className=" absolute  left-40  opacity-20 top-0   h-[600px] w-auto"
+        />
       </div>
     </div>
   );
